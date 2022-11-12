37e46147b1b04bf436fab0d29a7b11099db1a183
Aashna J Chandrashekhar <aashnajc@gmail.com>
2022-11-12 11:35:59 +0530

    ,

---
+++ DBMS/Weather Observation Station 6.sql
@@ -0,0 +1,5 @@
+select distinct city from station where city like 'a%';
+select distinct city from station where city like 'e%';
+select distinct city from station where city like 'i%';
+select distinct city from station where city like 'o%';
+select distinct city from station where city like 'u%';
